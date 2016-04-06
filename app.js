fb3121839e979bdfb24881506d5d0c553d3d6b48
honpery <honpery@gmail.com>
2016-04-06 23:58:37 +0800

    添加一个目录页

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var app = express();
 app.use(express.static('src'));
 
 
+app.get('/', function (req, res) {
+	res.sendFile('/src/index.html');
+
+});
+
+
 app.listen(3000, function () {
 	console.log("server running 3000");
 });
